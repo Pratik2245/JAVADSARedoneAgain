f5ce5429acdf4de28eaa7b08c09d07b06decd1c9
Pratik2245 <paithankarpratik3@gmail.com>
2025-02-20 23:45:57 +0530

    leetcode 441 question completed

--- LeetCodeQuestions/src/LeetCode_441ArrangingCoins.java
+++ LeetCodeQuestions/src/LeetCode_441ArrangingCoins.java
@@ -1,16 +1,33 @@
 public class LeetCode_441ArrangingCoins {
     public static void main(String[] args) {
-        int n=5;
-        arrangeCoins(n);
+        int n=8;
+       int ans= arrangeCoins(n);
+        System.out.println(ans);
     }
     public static int arrangeCoins(int n) {
-       int count=0;
-        for (int i = 1; i <=n ; i++) {
-            for (int j = i; j <=i ; j++) {
-                System.out.print(j);
+//       int ans=0;
+//        for (int j = 1; j <=n ; j++) {
+//            n=n-j;
+//            ans++;
+//            if(n<0){
+//                break;
+//            }
+//        }
+//        System.out.println(ans);
+//        another solution
+        int res=0;
+        int start=1,end=n;
+        while (start<=end){
+            int mid = start + (end - start) / 2; // Corrected mid calculation
+            long coins = (long) mid * (mid + 1) / 2;
+            if(coins>n){
+                end=mid-1;
+            }else{
+                start=mid+1;
+                res=Math.max(mid,res);
             }
-            System.out.println();
         }
-        return -1;
+        return res;
+
     }
 }
