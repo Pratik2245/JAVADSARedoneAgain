cc8949897bbfff18b76bb38fddc11e15419a2f01
Pratik2245 <paithankarpratik3@gmail.com>
2025-02-21 00:37:55 +0530

    LeetCode 1929 problem solved successfully

--- LeetCodeQuestions/src/LeetCode_1929ConcatenationOfArray.java
+++ LeetCodeQuestions/src/LeetCode_1929ConcatenationOfArray.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class LeetCode_1929ConcatenationOfArray {
     public static void main(String[] args) {
-
+//    int arr[]={1,2,1};
+    int arr[]={1,3,2,1};
+    int arr2[]=ConacateArray(arr);
+    System.out.println(Arrays.toString(arr2));
+    }
+    public static int[] ConacateArray(int arr[]){
+        int newArr[]=new int[arr.length*2];
+        int n=arr.length;
+        for (int i = 0; i <arr.length; i++) {
+            newArr[i]=arr[i];
+            newArr[i+arr.length]=arr[i];
+        }
+        return newArr;
     }
 }
