8d34beca462b0dc6e6d49872674ec3b66eda3b46
Pratik2245 <paithankarpratik3@gmail.com>
2025-03-05 22:59:52 +0530

    1285 string solution + optimised solution added successfully in the code successfully

--- LeetCodeQuestions/src/LeetCode_1295FindTheNumberWithEvenNumberOfDigits.java
+++ LeetCodeQuestions/src/LeetCode_1295FindTheNumberWithEvenNumberOfDigits.java
@@ -6,18 +6,29 @@ public class LeetCode_1295FindTheNumberWithEvenNumberOfDigits {
     }
     public static int findNumbers(int[] nums) {
         int count=0;
-        int calculatedCount=0;
         for(int i=0;i<nums.length;i++){
-            int temp=nums[i];
-            while (temp!=0){
-                int digit=temp%10;
-                calculatedCount+=1;
-                temp=temp/10;
-            }
-            if(calculatedCount%2==0){
+            int result=countDigits(nums[i]);
+            if(result%2==0){
                 count+=1;
             }
         }
+//        now optimised approach
+//        for (int num:nums){
+//            if(String.valueOf(num).length()%2==0){
+//                count+=1;
+//            }
+//        }
         return count;
     }
+    public static int countDigits(int num) {
+        int count = 0;
+        while (num > 0) {
+            num /= 10; // Remove the last digit
+            count++;    // Increase the count
+        }
+        return count;
+    }
+
+
+
 }
