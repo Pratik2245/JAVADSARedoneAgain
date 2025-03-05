a3eff15e219bf4cc6912d994759f84d8f777bc9e
Pratik2245 <paithankarpratik3@gmail.com>
2025-03-05 08:54:39 +0530

    String Mutability

---
+++ JAVA_VIT/src/StringMutability/P.java
@@ -0,0 +1,14 @@
+package StringMutability;
+
+public class P {
+    String name;
+    int x;
+    public static void main(String[] args) {
+        P p1=new P();
+        p1.name="CR";
+        P p2=p1;
+        p1.name="ACR";
+        System.out.println(p1.name);
+        System.out.println(p2.name);
+    }
+}
