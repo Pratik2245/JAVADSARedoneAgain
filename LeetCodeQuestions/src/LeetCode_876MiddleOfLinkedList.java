6d1422cea784fbdbe387da997a19dda189a10401
Pratik2245 <paithankarpratik3@gmail.com>
2025-03-29 23:13:54 +0530

    middle of linked list 876 done successfully

--- LeetCodeQuestions/src/LeetCode_876MiddleOfLinkedList.java
+++ LeetCodeQuestions/src/LeetCode_876MiddleOfLinkedList.java
@@ -1,2 +1,15 @@
 public class LeetCode_876MiddleOfLinkedList {
+    public static void main(String[] args) {
+//        class Solution {
+//            public ListNode middleNode(ListNode head) {
+//                ListNode slow=head;
+//                ListNode fast=head;
+//                while(fast!=null && fast.next!=null){
+//                    slow=slow.next;
+//                    fast=fast.next.next;
+//                }
+//                return slow;
+//            }
+//        }
+    }
 }
