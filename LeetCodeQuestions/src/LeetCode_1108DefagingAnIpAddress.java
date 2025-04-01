27f01d9e2558c55b8f04f394f1c9aace428f8c73
Pratik2245 <paithankarpratik3@gmail.com>
2025-04-01 22:01:29 +0530

    leetcode 1108 done successfully

---
+++ LeetCodeQuestions/src/LeetCode_1108DefagingAnIpAddress.java
@@ -0,0 +1,21 @@
+public class LeetCode_1108DefagingAnIpAddress {
+    public static void main(String[] args) {
+        String s= "1.1.1.1";
+       String str= defangIPaddr(s);
+        System.out.println(str);
+//        Output: "1[.]1[.]1[.]1"
+    }
+    public static String defangIPaddr(String address) {
+        StringBuilder s=new StringBuilder();
+
+        for (char ch:address.toCharArray()) {
+            if(ch=='.'){
+                s.append("[.]");
+
+            }else {
+                s.append(ch);
+            }
+        }
+        return s.toString();
+    }
+}
