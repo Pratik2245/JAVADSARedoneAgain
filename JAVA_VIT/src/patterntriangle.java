4f5030d0fa7c1e64957a1b389a993766b95b906f
Pratik2245 <paithankarpratik3@gmail.com>
2025-04-03 22:44:21 +0530

    LeetCode_557ReverseWordsinaStringIII.java done

--- JAVA_VIT/src/patterntriangle.java
+++ JAVA_VIT/src/patterntriangle.java
@@ -1,12 +1,17 @@
 public class patterntriangle {
     public static void main(String[] args) {
         int n=5;
+        int z=1;
         for (int i = 5; i >=1 ; i--) {
             for (int j = 0; j <=i; j++) {
                 if(j<i-1){
                     System.out.print("_");
                 }else if (j==i){
                     System.out.print("*");
+                    for (int k = 0; k <z ; k++) {
+                        System.out.print("_");
+                    }
+
                 }
             }
             System.out.println();
